4a74ec66a32cecd47c4555ef45f0e3066de91dda
markus <solbach.m@gmail.com>
2014-04-16 20:19:38 +0200

    First alpha of implementation

--- compMain.m
+++ compMain.m
@@ -2,19 +2,18 @@
 x = [0, 0, 0, 0, 0, 0];
 
 % Measurement
-y = [1, 0, 0, 0, 0, 0];
+y = [0, 0, 0, 0*pi/180, 0, 0];
 
 dt = 1;
 tt = 0:dt:10;
 
 % Loop
 for t = tt
-    x = comp(x,y);
-    y(1) = t;
     
-    
+    x = comp(x,y)
+        
     pause(0.1);
-    plot3(x(1), x(2), x(3), '.');
+    %plot3(x(1), x(2), x(3), '.');
 end
 
 
