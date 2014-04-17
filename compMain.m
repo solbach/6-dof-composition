c5d8678d2c4bde5f700114623176e2fd7b1d6813
markus <solbach.m@gmail.com>
2014-04-17 14:04:00 +0200

    Simulation alpha

--- compMain.m
+++ compMain.m
@@ -4,19 +4,33 @@ x = [0, 0, 0, 0, 0, 0];
 % Measurement (Odometry)
 y = [2, 0, 0, 0*pi/180, 0*pi/180, 90*pi/180];
 dt = 1;
-tt = 0:dt:0;
+tt = 0:dt:10;
+
+vX = 0;
+vY = 0;
+vZ = 0;
 
 % Loop
 for t = tt
     
-    y = [0, 0, 0, 0*pi/180, 0*pi/180, 45*pi/180];
-    x = comp(x,y)
+    % I.    Rotate
+    y = [0, 0, 0, 0*pi/180, 10*pi/180, 4*pi/180];
+    x = comp(x,y);
     
+    % II.   Translate
     y = [1, 0, 0, 0*pi/180, 0*pi/180, 0*pi/180];
-    x = comp(x,y)
-    %angle = x(6)*(180/pi)
-    pause(0.1);
-    %plot3(x(1), x(2), x(3), '.');
+    x = comp(x,y);
+    
+    % III.  Save Data to plot them
+    vX = [vX x(1)];
+    vY = [vY x(2)];
+    vZ = [vZ x(3)];
+    
 end
 
-
+   % vX = linspace(0,2*pi, 10)';
+   % vY = sin (vX);
+   % vZ = cos (vX);
+   % plot_dir3(vX, vY, vZ);
+    
+plot_dir3(vX', vY', vZ');
---
+++ plot_dir3.m
@@ -0,0 +1,69 @@
+function [h1, h2] = plot_dir3 (vX, vY, vZ)
+%function [h1, h2] = plot_dir3 (vX, vY, vZ)
+%Plotting x-y variables with direction indicating vector to the next element.
+%Example
+%   vX = linspace(0,2*pi, 10)';
+%   vY = sin (vX);
+%   vZ = cos (vX);
+%   plot_dir3(vX, vY, vZ);
+
+rMag = 0.5;
+
+% Length of vector
+lenTime = length(vX);
+
+% Indices of tails of arrows
+vSelect0 = 1:(lenTime-1);
+% Indices of tails of arrows
+vSelect1 = vSelect0 + 1;
+
+% X coordinates of tails of arrows
+vXQ0 = vX(vSelect0, 1);
+% Y coordinates of tails of arrows
+vYQ0 = vY(vSelect0, 1);
+% X coordinates of tails of arrows
+vZQ0 = vZ(vSelect0, 1);
+
+% X coordinates of heads of arrows
+vXQ1 = vX(vSelect1, 1);
+% Y coordinates of heads of arrows
+vYQ1 = vY(vSelect1, 1);
+% Z coordinates of heads of arrows
+vZQ1 = vZ(vSelect1, 1);
+
+% vector difference between heads & tails
+vPx = (vXQ1 - vXQ0) * rMag;
+vPy = (vYQ1 - vYQ0) * rMag;
+vPz = (vZQ1 - vZQ0) * rMag;
+
+% make plot 
+h1 = plot3 (vX, vY, vZ, '.-'); hold on;
+% add arrows 
+h2 = quiver3 (vXQ0, vYQ0, vZQ0, vPx, vPy, vPz, 0, 'r'); grid on; hold off
+axis equal
+
+
+% Copyright (c) 2010, Kangwon Lee
+% All rights reserved.
+
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+
+%     * Redistributions of source code must retain the above copyright
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in
+%       the documentation and/or other materials provided with the distribution
+
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
