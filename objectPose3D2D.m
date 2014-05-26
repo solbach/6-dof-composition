a9bd3ef5a4b1857465981d74e495d83a72ef68e7
markus <markus@t430.(none)>
2014-05-26 13:44:47 +0200

    minor bugfix: quaternion representation of matlab is not conform with the one in this work. And (c) added

--- objectPose3D2D.m
+++ objectPose3D2D.m
@@ -1,5 +1,7 @@
-function [rvec, tvec] = objectPose3D2D(ObjP, ImgP)
+function [rvec, tvec, q] = objectPose3D2D(ObjP, ImgP)
 % Find the object pose from 3D-2D point correspondences using the RANSAC scheme
+% INPUT : Object Points in 3D ( Nx3 ) and Image Points in 2D ( Nx2 )
+% OUTPUT: rotation matrix, translation vector and rotation as quaternion
 
 % Camera Matrix
     K = [749.642742046463, 0.0, 539.67454188334; 0.0, 718.738253774844, 410.819033898981; 0.0, 0.0, 1.0];
@@ -8,6 +10,38 @@ function [rvec, tvec] = objectPose3D2D(ObjP, ImgP)
 % using MexOpenCV an alternative could be POSIT
 % (http://www.cfar.umd.edu/~daniel/Site_2/Code.html)
     [rvec, tvec] = cv.solvePnPRansac(ObjP, ImgP, K);
-
+    rvec = cv.Rodrigues(rvec);
+    q = dcm2quat(rvec);
+    
+%   In the quaternion representation of this work w is in the beginning
+%   and not at the end.
+    w = q(1);
+    q(1) = q(4);
+    q(4) = w;
+    
  end
 
+% Copyright (c) 2014, Markus Solbach
+% All rights reserved.
+
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+
+%     * Redistributions of source code must retain the above copyright
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in
+%       the documentation and/or other materials provided with the distribution
+
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
--- stereoMatching.m
+++ stereoMatching.m
@@ -8,12 +8,40 @@ function P3 = stereoMatching(I1, I2)
 %     II. Find Correspondencies (with outlier elimination)
     [inlierPtsLeft, inlierPtsRight] = findCorrespondencies(f1, vpts1, f2, vpts2);
 
-%     III. Calculate 3D Point for each Correspondency
-    p3 = calculate3DPoint( inlierPtsLeft(1).Location, inlierPtsRight(1).Location );
-    for i = 2:inlierPtsLeft.Count
-        p3 = [ p3 calculate3DPoint(inlierPtsLeft(i).Location, ...
-                                   inlierPtsRight(i).Location) ];
+%     III. Calculate 3D Point for each Correspondency    
+    P3 = zeros(inlierPtsLeft.Count, 3);
+    
+    for i = 1:inlierPtsLeft.Count
+        pTemp = calculate3DPoint(inlierPtsLeft(i).Location, ...
+                                   inlierPtsRight(i).Location);
+        P3(i,1) = pTemp(1);
+        P3(i,2) = pTemp(2);
+        P3(i,3) = pTemp(3);
     end
-
-%     cv.solvePnPRansac;
+    
 end
+
+% Copyright (c) 2014, Markus Solbach
+% All rights reserved.
+
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+
+%     * Redistributions of source code must retain the above copyright
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in
+%       the documentation and/or other materials provided with the distribution
+
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
